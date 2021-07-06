132d40ad98e293410ffce70ab29d49f17a579baf
Alexey <nhevgevgev@gmail.com>
2021-07-06 22:22:18 +0300

    Implement controllers with models to Comment
    Add Methods:
    - GetCommentsByStudentId
    - AddComment
    - UpdateComment
    - RemoveComment

---
+++ EJournalDAL/Services/ICommentService.cs
@@ -0,0 +1,15 @@
+﻿using DataModels;
+using EJournalDAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EJournalDAL.Services
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<Comment>> GetCommentsByStudentId(int studentId);
+        Task<bool> AddComment(Comment commet, int studentId);
+        Task<bool> UpdateComment(Comment comment);
+        Task<bool> DeleteComment(int commentId);
+    }
+}
