a64f74ced0729eace3a8109622521464f520ef7b
Konstantin <murashko.konst.andr@gmail.com>
2021-07-12 15:07:25 +0300

    Add Controller for lessons

---
+++ EJournal-ASP.Net/Controllers/LessonController.cs
@@ -0,0 +1,53 @@
+﻿using EJournalDAL.Models;
+using EJournalDAL.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EJournal_ASP.Net.Controllers
+{
+    [Route("lesson/[controller]")]
+    [ApiController]
+    public class LessonController : ControllerBase
+    {
+        private readonly ILogger<LessonController> _logger;
+        private readonly ILessonService _lessonService;
+
+        public LessonController(ILessonService lessonService, ILogger<LessonController> logger)
+        {
+            _lessonService = lessonService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Lesson>> GetAllAsync()
+        {
+            return await _lessonService.GetAllLessons();
+        }
+
+        [HttpGet("lessonByIdGroup")]
+        public async Task<IEnumerable<Lesson>> GetLessonByIdAsync(int groupId)
+        {
+            return await _lessonService.GetLessonByGroupId(groupId);
+        }
+
+        [HttpPost]
+        public async Task<int?> AddAsync(Lesson lesson)
+        {
+            return await _lessonService.AddLesson(lesson);
+        }
+
+        [HttpDelete]
+        public async Task<bool> DeleteAsync(int lessonId)
+        {
+            return await _lessonService.DeleteLesson(lessonId);
+        }
+
+        [HttpPut]
+        public async Task<bool> UpdateAsync(Lesson lesson)
+        {
+            return await _lessonService.UpdateLesson(lesson);
+        }
+    }
+}
---
+++ EJournalDAL/Services/ILessonService.cs
@@ -0,0 +1,15 @@
+﻿using EJournalDAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EJournalDAL.Services
+{
+    public interface ILessonService
+    {
+        Task<IEnumerable<Lesson>> GetAllLessons();
+        Task<IEnumerable<Lesson>> GetLessonByGroupId(int idGroup);
+        Task<int> AddLesson(Lesson lesson);
+        Task<bool> UpdateLesson(Lesson lesson);
+        Task<bool> DeleteLesson(int lessonId);
+    }
+}
