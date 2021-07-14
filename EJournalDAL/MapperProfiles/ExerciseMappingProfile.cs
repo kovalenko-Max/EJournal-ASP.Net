2921ec21976a47de09d4794b691fadb5ec014e13
Maxim <aryewar@gmail.com>
2021-07-14 12:27:17 +0300

    Fix ExerciseMappingProfile

--- EJournalDAL/MapperProfiles/ExerciseMappingProfile.cs
+++ EJournalDAL/MapperProfiles/ExerciseMappingProfile.cs
@@ -8,8 +8,35 @@ namespace EJournalDAL.MapperProfiles
     {
         public ExerciseMappingProfile()
         {
-            CreateMap<Exercise, GetExercisesByGroupResult>();
-            CreateMap<GetExercisesByGroupResult, Exercise>();
+            CreateMap<Exercise, GetExercisesByGroupResult>()
+                .ForPath(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForPath(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForPath(dest => dest.Deadline, opt => opt.MapFrom(src => src.Deadline))
+                .ForPath(dest => dest.IdGroup, opt => opt.MapFrom(src => src.IdGroup))
+                .ForPath(dest => dest.Point, opt => opt.MapFrom(src => src))
+                .ForPath(dest => dest.ExerciseType, opt => opt.MapFrom(src => src.ExerciseType));
+            CreateMap<StudentMark, GetExercisesByGroupResult>()
+                .ForPath(dest => dest.IdStudent, opt => opt.MapFrom(src => src))
+                .ForPath(dest => dest.Point, opt => opt.MapFrom(src => src.Point));
+            CreateMap<Student, GetExercisesByGroupResult>()
+                .ForPath(dest => dest.IdStudent, opt => opt.MapFrom(src => src.Id))
+                .ForPath(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForPath(dest => dest.Surname, opt => opt.MapFrom(src => src.Surname));
+
+            CreateMap<GetExercisesByGroupResult, Exercise>()
+                .ForPath(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForPath(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForPath(dest => dest.Deadline, opt => opt.MapFrom(src => src.Deadline))
+                .ForPath(dest => dest.IdGroup, opt => opt.MapFrom(src => src.IdGroup))
+                .ForPath(dest => dest.StudentMarks, opt => opt.MapFrom(src => src))
+                .ForPath(dest => dest.ExerciseType, opt => opt.MapFrom(src => src.ExerciseType));
+            CreateMap<GetExercisesByGroupResult, StudentMark>()
+                .ForPath(dest => dest.Student, opt => opt.MapFrom(src => src))
+                .ForPath(dest => dest.Point, opt => opt.MapFrom(src => src.Point));
+            CreateMap<GetExercisesByGroupResult, Student>()
+                .ForPath(dest => dest.Id, opt => opt.MapFrom(src => src.IdStudent))
+                .ForPath(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForPath(dest => dest.Surname, opt => opt.MapFrom(src => src.Surname));
         }
     }
 }
--- EJournalDAL/Models/Exercise.cs
+++ EJournalDAL/Models/Exercise.cs
@@ -10,7 +10,7 @@ namespace EJournalDAL.Models
         public DateTime? Deadline { get; set; }
         public int? IdGroup { get; set; }
         public ExcerciseType ExerciseType { get; set; }
-        public List<StudentMark> StudentMarks { get; set; }
+        public StudentMark StudentMarks { get; set; }
 
         public Exercise()
         {
@@ -22,7 +22,7 @@ namespace EJournalDAL.Models
             Description = string.Empty;
             IdGroup = group.Id;
             ExerciseType = (ExcerciseType)0;
-            StudentMarks = new List<StudentMark>();
+            StudentMarks = new StudentMark();
         }
     }
 }
